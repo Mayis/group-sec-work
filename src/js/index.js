3321601954bc1ff75337dd52cc0be426cb308f16
Mayis <saroyan.mayis.ait@gmail.com>
2022-11-02 20:45:42 +0400

    group 2 slider work

--- src/js/index.js
+++ src/js/index.js
@@ -3,22 +3,22 @@ import fadeInOutParent from "./fadeIn-fadeOut slider/fadeInOutParent";
 import parentSlider from "./carusel-slider/parentSlider";
 
 // ---------------------------------------root HTML
-// const root = document.querySelector("#root");
-// // ---------------------select parameter
-// const width = 300;
-// const autoMode = false;
-// let counter = 3;
+const root = document.querySelector("#root");
+// ---------------------select parameter
+const width = 300;
+const autoMode = false;
+let counter = 3;
 
-// // --------------resize method
-// window.onresize = displayWindowSize;
-// window.onload = displayWindowSize;
-// function displayWindowSize() {
-//   const winWidth = window.innerWidth;
-//   if (winWidth < 1100) counter = 3;
-//   if (winWidth < 910) counter = 2;
-//   if (winWidth < 610) counter = 1;
+// --------------resize method
+window.onresize = displayWindowSize;
+window.onload = displayWindowSize;
+function displayWindowSize() {
+  const winWidth = window.innerWidth;
+  if (winWidth < 1100) counter = 3;
+  if (winWidth < 910) counter = 2;
+  if (winWidth < 610) counter = 1;
 
-//   root.innerHTML = "";
-//   parentSlider(root, data, width, counter, autoMode);
-// }
-fadeInOutParent(root, data);
+  root.innerHTML = "";
+  parentSlider(root, data, width, counter, autoMode);
+}
+// fadeInOutParent(root, data);
